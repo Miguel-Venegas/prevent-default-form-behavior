f4f37fe039d3a9143c5cb154894dfd397c14b828
Miguel-Venegas <venegas.a.miguel@gmail.com>
2021-01-06 09:15:53 -0800

    preventing default form behavior

---
+++ app.js
@@ -0,0 +1,22 @@
+
+const form = document.querySelector('#signup-form');
+
+const creditCardInput = document.querySelector('#cc');
+const termsCheckbox = document.querySelector('#terms');
+const veggieSelect = document.querySelector('#veggie');
+
+form.addEventListener('submit', function(e){
+    alert('Submitted the form');
+    console.log('cc', creditCardInput.value);
+    console.log('terms', termsCheckbox.checked);
+    console.log('veggie', veggieSelect.value);
+    e.preventDefault();
+
+});
+
+
+
+
+
+
+
